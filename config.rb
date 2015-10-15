1df52fea51e8e68f10e3d4f7fdffa7ccccaab197
Molly Puddister <molly@visiblemedia.com>
2015-10-15 17:16:57 -0400

    start website

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+
+#require 'toolkit'
+require 'sass-json-vars'
+
+#Folder settings
+relative_assets = true      #because we're not working from the root
+css_dir = "styles"          #where the CSS will saved
+sass_dir = "styles"           #where our .scss files are
+images_dir = "images"    #the folder with your images
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
