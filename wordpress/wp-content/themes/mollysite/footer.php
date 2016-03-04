aca821c30ad4412e53a17f87ea26a18e51e82086
mollypuddister <molly@visiblemedia.com>
2016-03-03 23:13:07 -0500

    update favicon, fix hover "Next", moda photo

--- wordpress/wp-content/themes/mollysite/footer.php
+++ wordpress/wp-content/themes/mollysite/footer.php
@@ -17,9 +17,9 @@
 
 		var slideRightBtn = document.querySelector('#c-button--slide-right');
 
-	 	slideRightBtn.onclick = function() {
+		slideRightBtn.onclick = function() {
 			slideRight.toggle();
-	 	};
+		};
 
 	</script>
 
@@ -45,6 +45,17 @@
 			$('#fullpage').fullpage();
 		});
 
+	</script>
+
+	<!-- google analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-73765710-1', 'auto');
+		ga('send', 'pageview');
 	</script>
 	
 </body>
--- wordpress/wp-content/themes/mollysite/header.php
+++ wordpress/wp-content/themes/mollysite/header.php
@@ -3,8 +3,7 @@
 <html>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<!-- <title>Molly Puddister</title> -->
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"> -->
+	<title><?php wp_title(''); ?></title>
 	<meta name="viewport" content="width=640, maximum-scale=1, user-scalable=no, target-densitydpi=device-dpi">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 
@@ -28,23 +27,23 @@
 
 
 	<!-- favicon -->
-	<link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="images/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
-	<link rel="manifest" href="images/favicon/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('siteurl'); ?>/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('siteurl'); ?>/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('siteurl'); ?>/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('siteurl'); ?>/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('siteurl'); ?>/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?php bloginfo('siteurl'); ?>/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#56fce5">
+	<meta name="msapplication-TileImage" content="<?php bloginfo('siteurl'); ?>/favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#56fce5">
 
 	<!-- jQuery -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
